357a714c9116965c4e3f4ce4c8485a7fb128a5d8
Darko Trifunovski <dtrifuno@gmail.com>
2020-07-05 12:26:22 -0500

    Switch to tabs

--- ch1/ex3/main.go
+++ ch1/ex3/main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "strings"
-    "os"
+	"fmt"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-    concatStart := time.Now()
-    s, sep := "", ""
-    for _, arg := range os.Args[1:] {
-        s += sep + arg
-        sep = " "
-    }
-    concatTime := time.Since(concatStart).Nanoseconds()
-    fmt.Printf("concat: %d ns\n", concatTime)
+	concatStart := time.Now()
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	concatTime := time.Since(concatStart).Nanoseconds()
+	fmt.Printf("concat: %d ns\n", concatTime)
 
-    joinStart := time.Now()
-    strings.Join(os.Args[1:], s)
-    joinTime := time.Since(joinStart).Nanoseconds()
-    fmt.Printf("strings.Join: %d ns\n", joinTime)
+	joinStart := time.Now()
+	strings.Join(os.Args[1:], s)
+	joinTime := time.Since(joinStart).Nanoseconds()
+	fmt.Printf("strings.Join: %d ns\n", joinTime)
 }
